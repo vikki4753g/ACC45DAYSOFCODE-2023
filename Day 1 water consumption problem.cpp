eee554dfa38754eb2e80de17419a363896bb09af
Vikas Patidar <116332152+vikki4753g@users.noreply.github.com>
2023-09-18 23:44:07 +0530

    Create Day 1 water consumption problem.cpp

---
+++ Day 1 water consumption problem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>=2000){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
