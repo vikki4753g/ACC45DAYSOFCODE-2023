f0fbd82e6786902da912547d22db9a0036cf24ad
Vikas Patidar <116332152+vikki4753g@users.noreply.github.com>
2023-11-01 23:40:20 +0530

    Create Day 44.cpp

---
+++ Day 44.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	   // int ans=1;
+	                   
+	    cout<<n*(n-1)<<endl;
+	}
+	return 0;
+}
